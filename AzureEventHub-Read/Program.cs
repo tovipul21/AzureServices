5df07244ac3d96e37d3e49de6213cac006ffe6b9
Vipul Mehta <tovipul21@hotmail.com>
2024-07-15 18:56:03 -0500

    Created new project to read the messages.

---
+++ AzureEventHub-Read/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureEventHub_Read
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
