21accf16a9f3b91d46d74b70c7e3e7114dfd9b65
Vipul Mehta <tovipul21@hotmail.com>
2024-07-20 20:31:29 -0500

    Refactored code to encapsulate the logic to retrive key data from config file within the KeyVault service class.

--- HelperServices/KeyVaultService.cs
+++ HelperServices/KeyVaultService.cs
@@ -6,28 +6,58 @@ namespace HelperServices
 {
     public class KeyVaultService
     {
+        IConfigurationBuilder _builder;
+        IConfigurationRoot _configurationRoot;
+        private string _keyVaultName = string.Empty;
+        private string _secretName = string.Empty;
+
+        public KeyVaultService()
+        {
+            _builder = new ConfigurationBuilder().AddJsonFile("appSettings.json", false, false);
+            _configurationRoot = _builder.Build();
+        }
+
         public string KeyVaultName 
         {
             get
             {
-                var builder = new ConfigurationBuilder().AddJsonFile("appSettings.json", false, false);
+                _keyVaultName = _configurationRoot["EventHubKeyVaultName"];
 
-                var configuration = builder.Build();
-
-                var keyVaultName = configuration["EventHubKeyVaultName"];
-
-                return keyVaultName == null ? "" : keyVaultName;
+                return _keyVaultName == null ? "" : _keyVaultName;
             }
         }
 
-        public async Task<string> RetriveSecretFromVaultAsync(string keyVaultName, string secretName)
+        public string SecretName 
+        { 
+            get
+            {
+                _secretName = _configurationRoot["SecretName"];
+
+                return _secretName == null ? "" : _secretName;
+            }
+        }
+
+        public async Task<string> RetriveSecretFromVaultAsync()
         {
             KeyVaultSecret kvs;
-            var keyVaultURI = $"https://{keyVaultName}.vault.azure.net/";
+            var keyVaultURI = $"https://{_keyVaultName}.vault.azure.net/";
 
             var client = new SecretClient(new Uri(keyVaultURI), new DefaultAzureCredential());
 
-            kvs = await client.GetSecretAsync(secretName);
+            kvs = await client.GetSecretAsync(_secretName);
+
+            return kvs.Value;
+        }
+
+        public async Task<string> SetSecretIntoVaultAsync(string newSecretValue)
+        {
+            KeyVaultSecret kvs;
+
+            var keyVaultURI = $"https://{_keyVaultName}.vault.azure.net/";
+
+            var client = new SecretClient(new Uri(keyVaultURI), new DefaultAzureCredential());
+
+            kvs = await client.SetSecretAsync(_secretName, newSecretValue);
 
             return kvs.Value;
         }
