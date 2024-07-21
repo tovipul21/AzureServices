d5d0489142d45561f2b4da7678d141750fcb878a
Vipul Mehta <tovipul21@hotmail.com>
2024-07-21 15:49:04 -0500

    Added a new project - AzureServiceBus-Send.csproj to the solution. This project is to explore various Azure ServiceBus features.

---
+++ AzureServiceBus-Send/Order.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace AzureServiceBus_Send
+{
+    internal class Order
+    {
+        public int OrderId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public decimal Total 
+        { 
+            get
+            {
+                return Quantity * UnitPrice;
+            }   
+        }
+
+        public decimal TotalValue 
+        { 
+            get; set; 
+        }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
---
+++ AzureServiceBus-Send/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using HelperServices;
+using Azure.Messaging.ServiceBus;
+
+namespace AzureServiceBus_Send
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await SendMessageToServiceBusQueue();
+        }
+
+        static async Task SendMessageToServiceBusQueue()
+        {
+            IConfigurationBuilder _builder = new ConfigurationBuilder().AddJsonFile("appSettings.json", false, false);
+            IConfigurationRoot _configurationRoot;
+            _configurationRoot = _builder.Build();
+
+            var queueName = _configurationRoot["ServiceBusQueueName"];
+            KeyVaultService kvService = new KeyVaultService();
+            
+            string eventHubConnectionString = await kvService.RetriveSecretFromVaultAsync();
+
+            List<Order> orders = new List<Order>() 
+            { 
+                new Order { OrderId = new Random().Next(0, 100), Quantity = new Random().Next(0, 10), UnitPrice = 23.20M },
+                new Order { OrderId = new Random().Next(0, 100), Quantity = new Random().Next(0, 10), UnitPrice = 42.55M }
+            };
+
+            //Console.WriteLine($"Service Bus Queue Send Connectionstring = {eventHubConnectionString}");
+
+            ServiceBusClient _client = new(eventHubConnectionString);
+
+            ServiceBusSender _sender = _client.CreateSender(queueName);
+
+            foreach (var order in orders)
+            {
+                ServiceBusMessage serviceBusMessage = new ServiceBusMessage(order.ToString());
+
+                _sender.SendMessageAsync(serviceBusMessage).GetAwaiter().GetResult();
+            }
+
+            Console.WriteLine($"All the {orders.Count} messages has been sent.");
+        }
+    }
+}
